386b94a942f2678bbdc9e5b31b1cde471b58149d
hardikyadav12 <hardik.ind12@gmail.com>
2024-05-19 06:21:59 +0530

    tryign to shutdown the consumer gracefully when program is terminated

---
+++ kafka-spring-boot/kafka-basics/src/main/java/io/kafka/demo/ConsumerDemoWithShutdown.java
@@ -0,0 +1,48 @@
+package io.kafka.demo;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class ConsumerDemoWithShutdown {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemoWithShutdown.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("hello consumer!");
+
+        //Create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "my-java-application");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+
+        //Create Producer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        //subscribe to the topic
+        consumer.subscribe(List.of("demo_topic"));
+
+        // poll for data
+        while(true) {
+            log.info("Polling");
+
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(1000));
+            for(ConsumerRecord<String, String> consumerRecord: consumerRecords) {
+                System.out.println(consumerRecord.value());
+            }
+        }
+    }
+}
